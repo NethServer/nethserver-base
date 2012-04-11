47bc83f0c01b060be9a12a82a5de83527323ba16
Davide Principi <davide.principi@nethesis.it>
2012-04-11 14:55:32 +0200

    RemoteAccess UI module: splitted language catalogs for plugins. Refs #1011 - in project:nethserver-httpd-admin

--- root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_RemoteAccess.php
+++ root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_RemoteAccess.php
@@ -2,21 +2,13 @@
 /**
  */
 
-$L['PPTP Settings'] = 'Configurazione PPTP';
-$L['client_label'] = 'Numero di client PPTP';
-$L['Remote management'] = 'Gestione remota';
 $L['Network address'] = 'Indirizzo di rete';
 $L['Network mask'] = 'Maschera di rete';
-$L['FTP settings'] = 'Configurazione FTP';
 $L['Allow access from any network'] = 'Consenti l\'accesso da qualsiasi rete';
 $L['Use password authentication'] = 'Utilizza l\'autenticazione con password';
 $L['Allow access from local network'] = 'Consenti l\'accesso da rete locale';
 
-$L['Pptp_Title'] = 'Pptp';
 $L['RemoteAccess_Title'] = 'Accesso remoto';
-$L['RemoteManagement_Title'] = 'Accesso web';
-$L['Ssh_Title'] = 'SSH';
-$L['Ftp_Title'] = 'Ftp';
 
 $L['status_anyNetwork_label'] = 'Accetta connessioni da qualsiasi rete';
 $L['status_localNetwork_label'] = 'Accetta connessioni solo da rete locale';
@@ -25,26 +17,17 @@ $L['acceptPasswordFromAnyNetwork_label'] = 'Consenti l\'uso della password da qu
 $L['address_label'] = 'Indirizzo di rete';
 $L['mask_label'] = 'Maschera di rete';
 
-$L['You should disable PPTP service'] = 'Dovresti disabilitare il servizio PPTP';
-$L['DisablePptp_label'] = 'Disabilita Pptp';
-
-$L['NethServer\Module\RemoteAccess_RemoteManagement_caption'] = 'Gestione remota';
-
 $L['0_label'] = $L['Network mask'];
 $L['Key_label'] = $L['Network address'];
-
-$L['Connection'] = 'Connessione';
-$L['port_label'] = 'Porta TCP';
-$L['access_public_label'] = 'Accetta da qualsiasi rete';
-$L['access_private_label'] = 'Accetta solo da rete locale';
 
 $L['Permissions'] = 'Permessi';
 $L['passwordAuth_label'] = 'Consenti l\'autenticazione mediante password';
 $L['rootLogin_label'] = 'Consenti l\'accesso per l\'utente root';
 $L['Authorize network'] = 'Rete affidabile';
 
-$L['remoteaccess-update_success'] = 'Configurazione di Accesso Remoto salvata correttamente';
-$L['remoteaccess-update_failure'] = 'Errore di configurazione durante il savataggo di Accesso Remoto';
 $L['read_Title'] = 'Accesso al server-manager';
 $L['read_Title_Description'] = 'L\'accesso al server-manager è consentito solo dalle reti specificate';
 $L['RemoteAccess_Tags'] = 'accesso remoto maschera rete ssh accesso password';
+
+$L['HttpdAdmin_Title'] = 'Accesso web';
+$L['Ssh_Title'] = 'SSH';
---
+++ root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_RemoteAccess_Ssh.php
@@ -0,0 +1,10 @@
+<?php
+/** 
+ */
+
+$L['Ssh_Title'] = 'SSH';
+
+$L['Connection'] = 'Connessione';
+$L['port_label'] = 'Porta TCP';
+$L['access_public_label'] = 'Accetta da qualsiasi rete';
+$L['access_private_label'] = 'Accetta solo da rete locale';
