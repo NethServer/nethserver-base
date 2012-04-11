8481585da7805b2018b6d229dabfb34ce33f8aa1
Davide Principi <davide.principi@nethesis.it>
2012-04-11 14:56:36 +0200

    RemoteManagement plugin: moved template to project:nethserver-httpd-admin. Refs #1011

--- root/usr/share/nethesis/NethServer/Template/RemoteAccess/RemoteManagement.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-echo $view->panel()
-        ->insert($view->header('address')->setAttribute('template', 'Authorize network'))
-        ->insert($view->textInput('address'))
-        ->insert($view->textInput('mask'));
-        
-echo $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_CANCEL);
