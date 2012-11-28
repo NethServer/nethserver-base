aa089de4b6ca3da9b954b0b2b7fedc260ee6d50d
Davide Principi <davide.principi@nethesis.it>
2012-11-28 12:24:01 +0100

    Nethesis decorator template: added headerMenu section with link to UserProfile module. Refs #659

--- root/usr/share/nethesis/NethServer/Template/Nethesis.php
+++ root/usr/share/nethesis/NethServer/Template/Nethesis.php
@@ -34,17 +34,18 @@ $view
         <div id="allWrapper"><?php echo $view['notificationOutput'] ?>
             <?php if ( ! $view['disableHeader']): ?>
                 <div id="pageHeader">
+                    <div id="headerMenu"><a href="<?php echo $view->getModuleUrl('/UserProfile') ?>"><?php echo $T('User\'s profile'); ?></a></div>
                     <h1 id="ModuleTitle"><?php echo htmlspecialchars($view['moduleTitle']) ?></h1>
                     <div id="productTitle">NethServer</div>
                 </div>
             <?php endif; ?>
             <div id="pageContent">
                 <div class="primaryContent" role="main">
-                    <div id="CurrentModule"><?php echo $view['currentModuleOutput'] ?></div>
-                    <?php if ( ! $view['disableFooter']): ?><div id="footer"><p><?php echo htmlspecialchars($view['company'] . ' - ' . $view['address']) ?></p></div><?php endif; ?>
+                    <?php echo $view['currentModuleOutput'] ?>                    
                 </div>
                 <?php if ( ! $view['disableMenu']): ?><div class="secondaryContent" role="menu"><h2><?php echo htmlspecialchars($view->translate('Other modules')) ?></h2><?php echo $view['menuOutput'] . $view['logoutOutput'] ?></div><?php endif; ?>
             </div><?php echo $view['helpAreaOutput'] ?>
+            <?php if ( ! $view['disableFooter']): ?><div id="footer"><p><?php echo htmlspecialchars($view['company'] . ' - ' . $view['address']) ?></p></div><?php endif; ?>
         </div><?php echo $view->literal($view['Resource']['js']) ?>
     </body>
 </html>
