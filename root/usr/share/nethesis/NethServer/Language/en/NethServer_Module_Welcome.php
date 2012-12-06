5ab0b0e3ffe6a173143604e84426cc501c6a403e
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2012-12-06 11:07:42 +0100

    welcome module: add description, links and license

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Welcome.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Welcome.php
@@ -3,4 +3,10 @@
 $L['NethServer_Welcome_Header'] = 'Welcome to NethServer'; 
 $L['Welcome_Description'] = 'Welcome page'; 
 $L['Welcome_Tags'] = 'welcome'; 
-$L['Welcome_Title'] = 'Welcome'; 
+$L['Welcome_Title'] = 'Welcome';
+$L['Welcome_Body'] = 'ATTENTION: this is an ALPHA relase! NethServer is based on SME Server and CentOS. See official site for further details.';
+$L['License'] = 'License';
+$L['Links'] = 'Links';
+$L['official_site'] = 'NethServer official site';
+$L['technical_documentation'] = 'Technical documentation';
+ 
--- root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_Welcome.php
+++ root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_Welcome.php
@@ -3,4 +3,9 @@
 $L['NethServer_Welcome_Header'] = 'Benvenuti in NethServer'; 
 $L['Welcome_Description'] = 'Pagina di benvenuto'; 
 $L['Welcome_Tags'] = 'benvenuti'; 
-$L['Welcome_Title'] = 'Benvenuti'; 
+$L['Welcome_Title'] = 'Benvenuti';
+$L['Welcome_Body'] = 'ATTENZIONE! Questa è una versione ALPHA! NethServer è basato su SME Server e CentOS. Visita il sito ufficiale per maggiori dettagli.';
+$L['License'] = 'License';
+$L['Links'] = 'Links';
+$L['official_site'] = 'Sito ufficiale NethServer';
+$L['technical_documentation'] = 'Documentazione tecnica';
