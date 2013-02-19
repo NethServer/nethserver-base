0e23bc4a5c304fd933531c6fb4d4b89b9021bf94
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2013-02-19 11:27:55 +0100

    Dashboard: move default css and js to SystemStatus Template. Refs #583

--- root/usr/share/nethesis/NethServer/Template/Dashboard/SystemStatus.php
+++ root/usr/share/nethesis/NethServer/Template/Dashboard/SystemStatus.php
@@ -1,4 +1,33 @@
 <?php
+$view->includeFile('NethServer/Js/jquery.masonry.min.js');
+$view->useFile("css/jquery.jqplot.min.css");
+$view->useFile("js/jquery.jqplot.min.js");
+
+$view->includeCSS("
+  div.dashboard-item {
+    margin: 5px;
+    padding: 5px;
+    border: 1px solid #ccc;
+    width: 300px;
+  }
+  .dashboard-item dt {
+    float: left;
+    clear: left;
+    width: 100px;
+    text-align: right;
+    font-weight: bold;
+  }
+  .dashboard-item dt:after {
+    content: \":\";
+  }
+  .dashboard-item dd {
+    margin: 0 0 0 110px;
+    padding: 0 0 0.5em 0;
+  }
+  .dashboard-item table.jqplot-table-legend {
+    width:auto;
+  }
+");
 
 foreach($view->getModule()->getChildren() as $child) {
     echo $view->inset($child->getIdentifier());
--- root/usr/share/nethesis/NethServer/Template/Dashboard/SystemStatus/Resources.php
+++ root/usr/share/nethesis/NethServer/Template/Dashboard/SystemStatus/Resources.php
@@ -1,35 +1,6 @@
 <?php
 
-$view->includeFile('NethServer/Js/jquery.masonry.min.js');
-$view->useFile("css/jquery.jqplot.min.css");
-$view->useFile("js/jquery.jqplot.min.js");
 $view->useFile("js/jqplot.pieRenderer.min.js");
-
-$view->includeCSS("
-  div.dashboard-item {
-    margin: 5px;
-    padding: 5px;
-    border: 1px solid #ccc;
-    width: 300px;
-  }
-  .dashboard-item dt {
-    float: left;
-    clear: left;
-    width: 100px;
-    text-align: right;
-    font-weight: bold;
-  }
-  .dashboard-item dt:after {
-    content: \":\";
-  }
-  .dashboard-item dd {
-    margin: 0 0 0 110px;
-    padding: 0 0 0.5em 0;
-  }
-  .dashboard-item table.jqplot-table-legend {
-    width:auto;
-  }
-");
 
 echo "<div class='dashboard-item'>";
 echo $view->header()->setAttribute('template',$T('general_title'));
