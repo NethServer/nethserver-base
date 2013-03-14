643548a04128812abb717d14e781c80f0968d6b1
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2013-03-14 14:10:45 +0100

    Dashboard: clean up code

--- root/usr/share/nethesis/NethServer/Js/jquery.nethserver.dashboard.resources.js
+++
@@ -1,9 +0,0 @@
-(function ( $ ) {
-    $(document).ready(function() {
-         $.Nethgui.Server.ajaxMessage({
-                isMutation: false,
-                url: "/Dashboard/SystemStatus"
-            });
- 
-    });
-} ( jQuery ));
