4803b112ed6b4653642f11e32bdfdbf4212e7723
Davide Principi <davide.principi@nethesis.it>
2013-03-18 18:28:52 +0100

    RemoteAccess language catalogs imported from nethserver-openssh package.

---
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_RemoteAccess.php
@@ -0,0 +1,20 @@
+<?php 
+
+$L['0_label'] = 'Mask'; 
+$L['Allow access from any network'] = 'Allow access from any network'; 
+$L['Allow access from local network'] = 'Allow access from local network'; 
+$L['Confirm deletion of'] = 'Confirm deletion of'; 
+$L['HttpdAdmin_Title'] = 'Web access'; 
+$L['Key_label'] = 'Network'; 
+$L['NethServer\Module\RemoteAccess_RemoteManagement_caption'] = 'Remote management'; 
+$L['Network address'] = 'Network address'; 
+$L['Network mask'] = 'Network mask'; 
+$L['Remote management'] = 'Remote management'; 
+$L['RemoteAccess_Description'] = 'Configure remote access to the system'; 
+$L['RemoteAccess_Tags'] = 'remote access ssh web mask network'; 
+$L['RemoteAccess_Title'] = 'Remote Access'; 
+$L['Ssh_Title'] = 'SSH'; 
+$L['Use password authentication'] = 'Use password authentication'; 
+$L['address_label'] = 'Address'; 
+$L['mask_label'] = 'Mask'; 
+$L['port_label'] = 'Port'; 
