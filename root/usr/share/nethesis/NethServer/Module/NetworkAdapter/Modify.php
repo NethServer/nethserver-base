eeb10427d78c649d1b597e370a7fc9d4162cf62b
Davide Principi <davide.principi@nethesis.it>
2013-08-02 14:29:36 +0200

    NetworkAdapter UI module: validate interface changes with "interface-config" platform validator. Refs #2103

--- root/usr/share/nethesis/NethServer/Module/NetworkAdapter/Modify.php
+++ root/usr/share/nethesis/NethServer/Module/NetworkAdapter/Modify.php
@@ -36,13 +36,13 @@ class Modify extends \Nethgui\Controller\Table\Modify
     /**
      * @var Array list of valid roles
      */
-    private $roles = array('green','red');
+    private $roles = array('green', 'red');
 
     /**
-    * @var Array of information about nic.
-    * Fields: name, hwaddr, bus, model, driver, speed, link
-    * Eg: green,08:00:27:77:fd:be,pci,Intel Corporation 82540EM Gigabit Ethernet Controller (rev 02),e1000,1000,1
-    */
+     * @var Array of information about nic.
+     * Fields: name, hwaddr, bus, model, driver, speed, link
+     * Eg: green,08:00:27:77:fd:be,pci,Intel Corporation 82540EM Gigabit Ethernet Controller (rev 02),e1000,1000,1
+     */
     private $nicInfo = null;
 
     public function initialize()
@@ -51,7 +51,7 @@ class Modify extends \Nethgui\Controller\Table\Modify
             array('device', Validate::USERNAME, \Nethgui\Controller\Table\Modify::KEY),
             array('hwaddr', Validate::MACADDRESS, \Nethgui\Controller\Table\Modify::FIELD),
             array('role', $this->getPlatform()->createValidator()->memberOf($this->roles), \Nethgui\Controller\Table\Modify::FIELD),
-            array('bootproto', $this->getPlatform()->createValidator()->memberOf(array('dhcp','static')), \Nethgui\Controller\Table\Modify::FIELD),
+            array('bootproto', $this->getPlatform()->createValidator()->memberOf(array('dhcp', 'static')), \Nethgui\Controller\Table\Modify::FIELD),
             array('ipaddr', Validate::IPv4_OR_EMPTY, \Nethgui\Controller\Table\Modify::FIELD),
             array('netmask', Validate::NETMASK_OR_EMPTY, \Nethgui\Controller\Table\Modify::FIELD),
             array('gateway', Validate::IPv4_OR_EMPTY, \Nethgui\Controller\Table\Modify::FIELD),
@@ -64,34 +64,32 @@ class Modify extends \Nethgui\Controller\Table\Modify
         parent::initialize();
     }
 
- 
     public function process()
     {
-        if($this->getIdentifier() === 'update') {
-            if(!$this->nicInfo) {
-                $this->nicInfo = str_getcsv($this->getPlatform()->exec('/usr/bin/sudo /usr/libexec/nethserver/nic-info '.$this->parameters['device'])->getOutput());
+        if ($this->getIdentifier() === 'update') {
+            if ( ! $this->nicInfo) {
+                $this->nicInfo = str_getcsv($this->getPlatform()->exec('/usr/bin/sudo /usr/libexec/nethserver/nic-info ' . $this->parameters['device'])->getOutput());
             }
         }
         parent::process();
     }
 
-
     public function prepareView(\Nethgui\View\ViewInterface $view)
     {
         parent::prepareView($view);
-        $view['roleDatasource'] =  array_map(function($fmt) use ($view) {
-            return array($fmt, $view->translate($fmt . '_label'));
-        }, $this->roles);
+        $view['roleDatasource'] = array_map(function($fmt) use ($view) {
+                return array($fmt, $view->translate($fmt . '_label'));
+            }, $this->roles);
 
-        if($this->getIdentifier() === 'update') {
-            $view['bus'] = isset($this->nicInfo[2])?$this->nicInfo[2]:"";
-            $view['model'] = isset($this->nicInfo[3])?$this->nicInfo[3]:"";
-            $view['driver'] = isset($this->nicInfo[4])?$this->nicInfo[4]:"";
-            $view['speed'] = isset($this->nicInfo[5])?$this->nicInfo[5]:"";
-            if (!isset($this->nicInfo[6]) || (intval($this->nicInfo[6]) < 0) ) {
+        if ($this->getIdentifier() === 'update') {
+            $view['bus'] = isset($this->nicInfo[2]) ? $this->nicInfo[2] : "";
+            $view['model'] = isset($this->nicInfo[3]) ? $this->nicInfo[3] : "";
+            $view['driver'] = isset($this->nicInfo[4]) ? $this->nicInfo[4] : "";
+            $view['speed'] = isset($this->nicInfo[5]) ? $this->nicInfo[5] : "";
+            if ( ! isset($this->nicInfo[6]) || (intval($this->nicInfo[6]) < 0)) {
                 $view['link'] = "N/A";
             } else {
-                $view['link'] = $this->nicInfo[6]?$view->translate('Yes'):$view->translate('No');
+                $view['link'] = $this->nicInfo[6] ? $view->translate('Yes') : $view->translate('No');
             }
         }
 
@@ -106,11 +104,11 @@ class Modify extends \Nethgui\Controller\Table\Modify
     public function validate(\Nethgui\Controller\ValidationReportInterface $report)
     {
         parent::validate($report);
-        $role = $this->getRequest()->getParameter('role');
-        $bootproto = $this->getRequest()->getParameter('bootproto');
-
-        if (strpos(is_string($role) ? $role : '','red') === FALSE && $bootproto === 'dhcp') {
-                $report->addValidationErrorMessage($this, 'bootproto', 'valid_bootproto_combination');
+        if ($this->getRequest()->isMutation()) {
+            $v = $this->createValidator()->platform('interface-config');
+            if ( ! $v->evaluate(json_encode($this->parameters->getArrayCopy()))) {                               
+                $report->addValidationError($this, 'device', $v);
+            }
         }
     }
 
