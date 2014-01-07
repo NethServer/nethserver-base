2b27c66299e624a1764ebac812536b86bcc5b0a8
Davide Principi <davide.principi@nethesis.it>
2014-01-07 15:19:37 +0100

    Added bash aliases for adm group commands executed by sudo. Refs #2492

--- root/etc/profile.d/nethserver.sh
+++ root/etc/profile.d/nethserver.sh
@@ -9,3 +9,18 @@ fi
 # Feature #1224 -- Date and time in Bash history
 export HISTTIMEFORMAT="%Y-%m-%d %T " 
 
+#
+# Define shortcut aliases for members of the adm group
+#
+if groups | grep -s '\badm\b' >/dev/null 2>/dev/null; then
+
+    admpath=/sbin/e-smith
+
+    alias db="sudo ${admpath}/db"
+    alias config="sudo ${admpath}/config"
+    alias signal-event="sudo ${admpath}/signal-event"
+    alias expand-template="sudo ${admpath}/expand-template"
+
+    unset admpath
+
+fi
