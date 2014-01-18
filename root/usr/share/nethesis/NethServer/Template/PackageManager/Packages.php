748641b2d4cc5b911a07eeeff36dee4778cf6527
Davide Principi <davide.principi@nethesis.it>
2014-01-18 15:29:51 +0100

    Added Help button to Packages UI module. Refs #2618 (comment#9)

--- root/usr/share/nethesis/NethServer/Template/PackageManager/Packages.php
+++ root/usr/share/nethesis/NethServer/Template/PackageManager/Packages.php
@@ -1,12 +1,13 @@
-<table class="SmallTable"><thead>
+<?php 
+/* @var $view \Nethgui\Renderer\Xhtml */
+$view->rejectFlag($view::INSET_FORM);
+echo $view->buttonList($view::BUTTON_HELP);
+
+?><table class="SmallTable"><thead>
         <tr style="border-bottom: 1px solid gray"><th><?php echo $T('rpm_name') ?></th>
             <th><?php echo $T('rpm_version')?></th>
             <th><?php echo $T('rpm_release') ?></th>
         </tr></thead><?php
-
-/* @var $view \Nethgui\Renderer\Xhtml */
-
-$view->rejectFlag($view::INSET_FORM);
 
 echo $view->objectsCollection('packages')
     ->setAttribute('tag', 'tbody')    
