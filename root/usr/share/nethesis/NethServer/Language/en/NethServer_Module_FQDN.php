e1a8a05fadf3d06b93c43103f21cbbcd713dc8b9
Davide Principi <davide.principi@nethesis.it>
2014-02-25 18:33:19 +0100

    FQDN UI module: fixed en label.

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_FQDN.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_FQDN.php
@@ -4,6 +4,6 @@ $L['FQDN_Description'] = 'Configure server host and domain name';
 $L['FQDN_Tags'] = 'hostname host name domainname domain fqdn'; 
 $L['FQDN_Title'] = 'Server name';
 $L['FQDN_header'] = 'Host and domain name';
-$L['FQDN_warning'] = 'Any change to these fields will re-creates the SSL certificates and reconfigure the entire system. The process might take a while.';
+$L['FQDN_warning'] = 'Any change to these fields re-creates the SSL certificates and reconfigure all installed packages. The process might take a while.';
 $L['SystemName_label'] = 'Hostname';
 $L['DomainName_label'] = 'Domain';
