51cd290255fd63077f215abdbc44366072538d13
Davide Principi <davide.principi@nethesis.it>
2014-03-10 17:48:12 +0100

    PackageManager: use relative module URLs. Refs #2692

--- root/usr/share/nethesis/NethServer/Template/PackageManager/Packages.php
+++ root/usr/share/nethesis/NethServer/Template/PackageManager/Packages.php
@@ -15,7 +15,7 @@ echo $view->objectsCollection('packages')
     ->setAttribute('key', 'name');
 
 $packagesTarget = $view->getClientEventTarget('packages');
-$moduleUrl = json_encode($view->getModuleUrl('/PackageManager/Packages'));
+$moduleUrl = json_encode($view->getModuleUrl('Packages'));
 
 $view->includeCss("
 table.SmallTable {width: auto;  font-size: 12px}
