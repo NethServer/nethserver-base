27ebf25d86363cc3c511ead47e84a80b00130c03
Davide Principi <davide.principi@nethesis.it>
2014-04-02 09:32:06 +0200

    Fixed IT translation typo

--- root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_FQDN.php
+++ root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_FQDN.php
@@ -4,6 +4,6 @@ $L['FQDN_Description'] = 'Configura il nome host e il dominio del server';
 $L['FQDN_Tags'] = 'hostname host name domainname domain fqdn dominio nome'; 
 $L['FQDN_Title'] = 'Nome server';
 $L['FQDN_header'] = 'Nome host e dominio';
-$L['FQDN_warning'] = 'Ogni modifica ai dati inseriti causerà la creazione di nuovi certificati ed una riconfigurazione dell\'interno sistema. L\'operazione potrebbe richiedere alcuni minuti.';
+$L['FQDN_warning'] = 'Ogni modifica ai dati inseriti causerà la creazione di nuovi certificati ed una riconfigurazione dell\'intero sistema. L\'operazione potrebbe richiedere alcuni minuti.';
 $L['SystemName_label'] = 'Nome host';
 $L['DomainName_label'] = 'Dominio';
