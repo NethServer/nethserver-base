ee4a10e8e6a7521d78cc6509ca1fb525859c8b59
Davide Principi <davide.principi@nethesis.it>
2015-01-16 16:39:32 +0100

    FirstConfigWiz: changed Next/Back buttons. Refs #2957

--- root/usr/share/nethesis/NethServer/Template/FirstConfigWiz/WizFooter.php
+++ root/usr/share/nethesis/NethServer/Template/FirstConfigWiz/WizFooter.php
@@ -2,8 +2,7 @@
 /* @var $view \Nethgui\Renderer\Xhtml */
 
 echo $view->buttonList($view::BUTTON_HELP)
-    ->insert($view->button('Save', $view::BUTTON_SUBMIT))
-    ->insert($view->button('Skip', $view::BUTTON_LINK)->setAttribute('value', $view->getModuleUrl(sprintf('../%s?skip', $view->getModule()->getIdentifier()))))
+    ->insert($view->button('Next', $view::BUTTON_SUBMIT))
     ->insert($view->button('Back', $view::BUTTON_CANCEL))
 ;
 
