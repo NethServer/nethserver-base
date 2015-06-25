c9b8aed6413029dd67a7f74ea875d0537bdb9c90
Davide Principi <davide.principi@nethesis.it>
2015-06-25 15:42:39 +0200

    TrustedNetworks.pm: added POD documentation. Refs #3195

--- lib/perl/NethServer/TrustedNetworks.pm
+++ lib/perl/NethServer/TrustedNetworks.pm
@@ -35,6 +35,72 @@ INIT {
     }
 }
 
+
+=head1 NAME
+
+NethServer::TrustedNetworks -- extensible module for trusted networks
+providers
+
+=head1 DESCRIPTION
+
+A network can be trusted for disparate reasons, and cannot be defined
+a priori.   This module defines an API to
+
+=over
+
+=item * register a custom "provider" function, that returns some trusted
+networks when invoked
+
+=item * retrieve the complete list of trusted networks from the registered
+providers
+
+=back
+
+To define a provider function, add a Perl module under
+TrustedNetworks/ directory, with namespace prefix
+NethServer::TrustedNetworks.
+
+For a real example, see the file
+Default.pm.
+
+=head1 USAGE
+
+This is an example provider "Provider1" definition.
+
+ package NethServer::TrustedNetworks::Provider1;
+ use NethServer::TrustedNetworks qw(register_callback);
+
+ register_callback(&provider1);
+
+ sub provider1
+ {
+    my $results = shift;
+    ... # do something with $results (array ref)
+ }
+
+User code example, that retrieves the list of trusted networks in CIDR
+format:
+
+ use NethServer::TrustedNetworks;
+
+ print join(",", NethServer::TrustedNetworks::list_cidr()) . "\n";
+
+=head1 API FUNCTIONS
+
+=over
+
+=item * register_callback($func_ref, $order = undef)
+
+=item * list_cidr() returns the trusted networks list with CIDR format
+(i.e. 192.168.1.0/24)
+
+=item * list_mask() returns the trusted networks list with netmask
+format (i.e 192.168.1.0/255.255.255.0)
+
+=back
+
+=cut
+
 sub register_callback
 {
     my $func = shift;
