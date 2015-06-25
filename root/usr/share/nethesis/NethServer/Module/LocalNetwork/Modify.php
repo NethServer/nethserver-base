feb4e5042a23ead0f8ad2eb2c6f38246a26802a6
Davide Principi <davide.principi@nethesis.it>
2015-06-25 18:29:24 +0200

    LocalNetwork UI module: use trusted-networks helper. Refs #3195

--- root/usr/share/nethesis/NethServer/Module/LocalNetwork/Modify.php
+++ root/usr/share/nethesis/NethServer/Module/LocalNetwork/Modify.php
@@ -33,16 +33,10 @@ class Modify extends \Nethgui\Controller\Table\Modify
 
     public function initialize()
     {
-        $columns = array(       
-            'Key',
-            'Mask',
-            'Description',
-            'Actions'
-        );
 
         $parameterSchema = array(
             array('network', Validate::IPv4, \Nethgui\Controller\Table\Modify::KEY),
-            array('Mask', Validate::IPv4, \Nethgui\Controller\Table\Modify::FIELD),
+            array('Mask', Validate::IPv4_NETMASK, \Nethgui\Controller\Table\Modify::FIELD),
             array('Description', Validate::ANYTHING, \Nethgui\Controller\Table\Modify::FIELD),
         );
 
@@ -51,58 +45,28 @@ class Modify extends \Nethgui\Controller\Table\Modify
         parent::initialize();
     }
 
-    private function maskToCidr($mask){
-        $long = ip2long($mask);
-        $base = ip2long('255.255.255.255');
-        return 32-log(($long ^ $base)+1,2);
-    }
-
-    private function ipInRange( $ip, $range ) {
-        if ( strpos( $range, '/' ) == false ) {
-                $range .= '/32';
-        }
-        // $range is in IP/CIDR format eg 127.0.0.1/24
-        list( $range, $netmask ) = explode( '/', $range, 2 );
-        $range_decimal = ip2long( $range );
-        $ip_decimal = ip2long( $ip );
-        $wildcard_decimal = pow( 2, ( 32 - $netmask ) ) - 1;
-        $netmask_decimal = ~ $wildcard_decimal;
-        return ( ( $ip_decimal & $netmask_decimal ) == ( $range_decimal & $netmask_decimal ) );
-    }
-
-
     public function validate(\Nethgui\Controller\ValidationReportInterface $report)
     {
         parent::validate($report);
-        if( $this->getRequest()->isMutation()) {
-            $net = long2ip(ip2long($this->parameters['network']) & ip2long($this->parameters['Mask']));
-            if ($net != $this->parameters['network']) {
-                $report->addValidationErrorMessage($this, 'network', 'invalid_network', array($this->parameters['network']));
-            }
-
-            // check the network is not already used
-            $interfaces = $this->getPlatform()->getDatabase('networks')->getAll();
-            foreach ($interfaces as $interface => $props) {
-                if(isset($props['role']) && isset($props['ipaddr']) ) {
-                    $cidr = $this->parameters['network']."/".$this->maskToCidr($this->parameters['Mask']);
-                    if ($this->ipInRange($props['ipaddr'], $cidr)) {
-                        $report->addValidationErrorMessage($this, 'network', 'used_network', array($this->parameters['network']));
-                    }
-                }
-            }
-
-        $openvpn = $this->getPlatform()->getDatabase('configuration')->getKey('openvpn');
-        if (isset($openvpn['Network']) && $openvpn['Network'] == $this->parameters['network']) {
-            $report->addValidationErrorMessage($this, 'network', 'used_network', array($this->parameters['network']));
+        if ( ! $this->getRequest()->isMutation() || $report->hasValidationErrors()) {
+            return;
         }
 
-        $ipsec = $this->getPlatform()->getDatabase('configuration')->getKey('ipsec');
-        if (isset($ipsec['L2tpNetwork']) && $ipsec['L2tpNetwork'] == $this->parameters['network']) {
-            $report->addValidationErrorMessage($this, 'network', 'used_network', array($this->parameters['network']));
+        // check the "network" parameter is consistent with its "Mask" (only 0-bits in tail)
+        $net = long2ip(ip2long($this->parameters['network']) & ip2long($this->parameters['Mask']));
+        if ($net != $this->parameters['network']) {
+            $report->addValidationErrorMessage($this, 'network', 'invalid_network', array($this->parameters['network']));
+            return;
         }
 
+        // check the network is not already used
+        $curr = $this->parameters['network'] . '/' . $this->parameters['Mask'];
+        foreach ($this->getParent()->getAdapter() as $net => $props) {
+            if ($curr === ($net . '/' . $props['Mask'])) {
+                $report->addValidationErrorMessage($this, 'network', 'used_network', array($this->parameters['network']));
+                return;
+            }
         }
     }
-
 
 }
