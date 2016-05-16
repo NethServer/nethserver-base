98b38dccc537ca3dd162d27cce29f582640eb673
Davide Principi <davide.principi@nethesis.it>
2016-05-16 16:17:18 +0200

    Fix EmailAddress label

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_UserProfile.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_UserProfile.php
@@ -1,8 +1,8 @@
 <?php 
 
+$L['EmailAddress_label'] = 'External email address';
 $L['FullName_label'] = 'Full name';
 $L['ExtraInfo_Title'] = 'Contact informations'; 
 $L['UserProfile_Description'] = 'Change user settings';
 $L['UserProfile_Header'] = '${0} user\'s profile';
 $L['UserProfile_Tags'] = 'user profile preferences city address email phone';
-$L['EmailAddress_label'] = 'Email address';
