eb08e0aaf2e011d7a9c58b2ea80398494ccc0987
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2016-05-17 16:07:21 +0200

    User profile: really disable change password

--- root/usr/share/nethesis/NethServer/Template/UserProfile/Personal.php
+++ root/usr/share/nethesis/NethServer/Template/UserProfile/Personal.php
@@ -8,11 +8,11 @@ echo $view->textInput('FullName', $view::STATE_DISABLED | $view::STATE_READONLY)
 
 if($view['username'] === 'root') {
     echo $view->textInput('EmailAddress');
-    $buttons = $view::BUTTON_SUBMIT | $view::BUTTON_HELP;
+    $buttonList = $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_HELP);
 } else {
-    $buttons = $view::BUTTON_HELP;
+    $buttonList = $view->buttonList($view::BUTTON_HELP);
+    if (!$view['readOnly']) {
+        $buttonList->insert($view->button('ChangePassword', $view::BUTTON_LINK));
+    }
 }
-
-echo $view->buttonList($buttons)
-    ->insert($view->button('ChangePassword', $view::BUTTON_LINK))
-;
+echo $buttonList;
