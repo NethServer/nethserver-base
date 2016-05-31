ea32e7ded790b6f82a1a2dc59f9adc66a2184cb9
AleRossini <alessia.rossini.91@gmail.com>
2016-05-31 16:34:33 +0200

    Refresh UI: move css to base.css (#25)

--- root/usr/share/nethesis/NethServer/Template/Dashboard/SystemStatus.php
+++ root/usr/share/nethesis/NethServer/Template/Dashboard/SystemStatus.php
@@ -1,45 +1,6 @@
 <?php
 $view->useFile('js/jquery.masonry.min.js');
 
-$view->includeCSS("
-  div.dashboard-item {
-    margin: 5px;
-    padding: 5px;
-    border: 1px solid #ccc;
-    width: 300px;
-    min-height: 50px;
-    background: #fff;
-  }
-  .dashboard-item dt {
-    float: left;
-    clear: left;
-    text-align: right;
-    font-weight: bold;
-    margin-right: 0.5em;
-    padding: 0.1em;
-  }
-  .dashboard-item dt:after {
-    content: \":\";
-  }
-  .dashboard-item dd {
-    text-align: right;
-    padding: 0.1em;
-  }
-  .dashboard-graph {
-    margin-top: 0.2em;
-    height:250px;
-    width:250px;
-  }
-  .dashboard-item h2 {
-    font-weight: bold;
-    font-size: 120%;
-    text-align: center;
-    border-radius: 0px;
-    margin: -5px -5px 5px -5px;
-    background: #ccc;
-  }
-");
-
 foreach($view->getModule()->getChildren() as $child) {
     echo $view->inset($child->getIdentifier());
 }
