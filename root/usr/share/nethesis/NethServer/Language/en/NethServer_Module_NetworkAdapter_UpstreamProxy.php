1dc1167ab4455b6992cccb52bb6bd7d7f38594f3
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2016-06-10 17:49:29 +0200

    FirstConfigWiz: remove UpstreamProxy

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_NetworkAdapter_UpstreamProxy.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_NetworkAdapter_UpstreamProxy.php
@@ -3,7 +3,6 @@
 $L['UpstreamProxy_label'] = 'Proxy';
 $L['UpstreamProxy_header'] = 'Upstream proxy';
 $L['UpstreamProxy_Title'] = 'Proxy settings';
-$L['UpstreamProxyStatus_label'] = 'Connect to Internet using a proxy server';
 $L['UpstreamProxy_Description'] = 'Change upstream proxy configuration';
 $L['UpstreamProxy_Action'] = 'Set upstream proxy';
 $L['host_label'] = 'HTTP(S) proxy';
