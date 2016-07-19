f53dcf0cd966877c8e8507e3ee62326f14bd4b16
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2016-07-19 14:11:13 +0200

    Web UI: add missing labels. NethServer/dev#5061

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Dashboard_Applications.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Dashboard_Applications.php
@@ -3,5 +3,7 @@
 /* NethServer_Module_Dashboard_Applications translation, language: en */
 
 $L['Applications_Title'] = 'Applications';
+$L['Applications_Tags'] = 'Applications application applications web';
+$L['Applications_Description'] = 'Installed applications';
 $L['Open_label'] = 'Open';
 $L['no_application_installed'] = 'No application installed';
