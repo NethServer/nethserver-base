755bb4c42f2036afc40697b951c25e24984a1fe4
dnutan <dnutan@users.noreply.github.com>
2016-10-16 22:56:26 +0200

    minor typo

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_UserProfile.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_UserProfile.php
@@ -2,7 +2,7 @@
 
 $L['EmailAddress_label'] = 'External email address';
 $L['FullName_label'] = 'Full name';
-$L['ExtraInfo_Title'] = 'Contact informations'; 
+$L['ExtraInfo_Title'] = 'Contact information'; 
 $L['UserProfile_Description'] = 'Change user settings';
 $L['UserProfile_Header'] = '${0} user\'s profile';
 $L['UserProfile_Tags'] = 'user profile preferences city address email phone';
