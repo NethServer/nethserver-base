22c1b38ab2a2f35cd0fb32bdc7e773db3929b628
Davide Principi <davide.principi@nethesis.it>
2018-03-30 10:50:23 +0200

    Protect against integer conversion
    Workaround the hashToDatasource() serialization code of Nethgui that casts keys to integers

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_TlsPolicy.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_TlsPolicy.php
@@ -3,5 +3,5 @@ $L['TlsPolicy_Title'] = 'TLS policy';
 $L['TlsPolicy_Description'] = 'Transport Layer Security hardening policy';
 $L['TlsPolicy_Tags'] = 'SSL encryption cipher protocol';
 $L['TlsPolicy_header'] = 'TLS policy';
-$L['policy_label'] = 'Enforced security level';
+$L['Policy_label'] = 'Enforced security level';
 $L['LEGACY'] = 'Legacy';
--- root/usr/share/nethesis/NethServer/Template/TlsPolicy.php
+++ root/usr/share/nethesis/NethServer/Template/TlsPolicy.php
@@ -3,7 +3,7 @@
 echo $view->header()->setAttribute('template', $T('TlsPolicy_header'));
 
 echo $view->panel()
-    ->insert($view->selector('policy', $view::SELECTOR_DROPDOWN))
+    ->insert($view->selector('Policy', $view::SELECTOR_DROPDOWN))
     ;
 
 echo $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_HELP);
