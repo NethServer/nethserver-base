46225bcff953a7a01096ba1e42c7b01d29b987f1
dnutan <dnutan@users.noreply.github.com>
2018-07-24 15:52:50 +0200

    mask password field
    (optional)

--- root/usr/share/nethesis/NethServer/Template/NetworkAdapter/SetPppoeParameters.php
+++ root/usr/share/nethesis/NethServer/Template/NetworkAdapter/SetPppoeParameters.php
@@ -6,7 +6,7 @@ echo $view->header()->setAttribute('template', $T('SetPppoeParameters_header'));
 echo $view->selector('PppoeInterface', $view::SELECTOR_DROPDOWN )->setAttribute('choices', 'PppoeInterfaceDatasource');
 echo $view->textInput('PppoeProvider');
 echo $view->textInput('PppoeUser');
-echo $view->textInput('PppoePassword');
+echo $view->textInput('PppoePassword', $view::TEXTINPUT_PASSWORD);
 echo $view->selector('PppoeAuthType', $view::SELECTOR_DROPDOWN)->setAttribute('choices', \Nethgui\Widget\XhtmlWidget::hashToDatasource(array('auto' => $T('AuthType_auto'), 'pap' => $T('AuthType_pap'), 'chap' => $T('AuthType_chap'))));
 
 echo $view->fieldset()->setAttribute('template', $T('multiwan_label'))
