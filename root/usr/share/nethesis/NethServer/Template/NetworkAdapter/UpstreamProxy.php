84d2fe673de533b8fe0e65a74b1d7f51996c6bbe
dnutan <dnutan@users.noreply.github.com>
2018-08-06 15:33:54 +0200

    mask password field (#128)
    NethServer/dev#5554

--- root/usr/share/nethesis/NethServer/Template/NetworkAdapter/UpstreamProxy.php
+++ root/usr/share/nethesis/NethServer/Template/NetworkAdapter/UpstreamProxy.php
@@ -6,7 +6,7 @@ echo $view->panel()
     ->insert($view->textInput('host'))
     ->insert($view->textInput('port'))
     ->insert($view->textInput('username'))
-    ->insert($view->textInput('password'))
+    ->insert($view->textInput('password', $view::TEXTINPUT_PASSWORD))
 ;
 
 echo $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_CANCEL | $view::BUTTON_HELP);
